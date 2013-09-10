ee1a3dba4ad89925863c62fbe0bbe6281460226d
K.Kolotyuk <kk@7bits.com>
2013-09-10 09:28:54 +0700

    JSON CRUD for topic model

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,28 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+    render json: @topics
+  end
+
+  def create
+    @topic = Topic.new(params[:topic])
+    @topic.save
+    render json: @topic
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+    render json: @topic
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.update_attributes(params[:topic])
+  end
+
+  def destroy
+    @product = Topic.find(params[:id])
+    @product.destroy
+    render head :no_content
+  end
+end
